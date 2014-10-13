7922ba635ae8377b392265b56050fbe77f45bd5c
Chris Wilson <cdw291@nyu.edu>
2014-10-13 09:53:22 -0400

    Adds functionality to get rid of very long response time trials and to get rid of initial trials within a given session.

--- analysis/reaction_time.py
+++ analysis/reaction_time.py
@@ -82,7 +82,7 @@ def plot_rxn(behavior_epochs):
     for epoch in behavior_epochs:
         rxn = epoch.reaction_times
         result = epoch.trials['result']
-        valid = (result <5) * (result > 0)
+        valid = (result < 5) * (result > 0)
         correct = result < 3
         rxn = rxn[valid]
         correct = correct[valid]
@@ -94,20 +94,22 @@ def plot_rxn(behavior_epochs):
     return c_rxn, c_correct
 
 
-def _sort_rxn(behavior_epochs):
+def _sort_rxn(behavior_epochs, skip_first=20):
     """
 
     :param behavior_epochs:
+    :param skip_first: number of initial trials to skip within a session.
     :return:
     """
     c_correct = np.array([], dtype=np.bool)
     c_rxn = np.array([],dtype=np.float64)
     for epoch in behavior_epochs:
+        trials = epoch.trials[skip_first:]
         if not hasattr(epoch, 'reaction_times'):
             rxn_time_epoch(epoch)
-        rxn = epoch.reaction_times
-        result = epoch.trials['result']
-        valid = (result <5) * (result > 0)
+        rxn = epoch.reaction_times[skip_first:]
+        result = trials['result']
+        valid = (result < 5) * (result > 0)
         correct = result < 3
         rxn = rxn[valid]
         correct = correct[valid]
@@ -127,18 +129,21 @@ def _sort_rxn(behavior_epochs):
     # Collapse duplicate observations:
     u_r = np.unique(rx)
     rx2 = np.zeros(u_r.size)
-    res = np.zeros((u_r.size,2))
+    res = np.zeros((u_r.size, 2))
     for i, r in enumerate(u_r):
         idx = rx==r
         n_obs = np.sum(idx)
         n_cor = np.sum(cor[idx])
-        res[i,:] = n_cor, n_obs
+        res[i ,:] = n_cor, n_obs
         rx2[i] = r
+    # get rid of very long reaction time trials:
+    res = res[rx2 < 250, :]
+    rx2 = rx2[rx2 < 250]
 
     return rx2, res
 
 
-def rxn_make_observer(behavior_epochs):
+def rxn_make_observer(behavior_epochs, **kwargs):
     """
     Helper function to parse reaction time data from behavior epochs.
 
@@ -153,7 +158,7 @@ def rxn_make_observer(behavior_epochs):
     return obs
 
 
-def rxn_concentration_make_observers(behavior_epochs):
+def rxn_concentration_make_observers(behavior_epochs, skip_first=20):
     concentrations = []
     conc = np.array([])
     for b in behavior_epochs:
@@ -163,26 +168,28 @@ def rxn_concentration_make_observers(behavior_epochs):
     observers = []
 
     for con in conc:
+
         c_correct = np.array([], dtype=np.bool)
         c_rxn = np.array([],dtype=np.float64)
         for epoch in behavior_epochs:
+            trials = epoch.trials[skip_first:]
             if not hasattr(epoch, 'reaction_times'):
                 rxn_time_epoch(epoch)
-            rxn = epoch.reaction_times
+            rxn = epoch.reaction_times[skip_first:]
             result = epoch.trials['result']
             concentrations = epoch.trials['odorconc']
-            valid = (result <5) * (result > 0) * (concentrations == con)
+            valid = (result < 5) * (result > 0) * (concentrations == con)
             correct = result < 3
             rxn = rxn[valid]
             correct = correct[valid]
             c_rxn = np.concatenate([c_rxn, rxn])
             c_correct = np.concatenate([c_correct, correct])
         # combine, sort by rxn time, and break back into two arrays.
-        c = np.hstack((c_rxn[:,np.newaxis],
-                              c_correct[:,np.newaxis]))
-        c = c[c[:,0].argsort()]
-        rx = c[:,0]
-        cor = c[:,1]
+        c = np.hstack((c_rxn[:, np.newaxis],
+                       c_correct[:, np.newaxis]))
+        c = c[c[:, 0].argsort()]
+        rx = c[:, 0]
+        cor = c[:, 1]
         # remove nans from this stuff:
         not_nans = ~np.isnan(rx)
         not_nans = ~np.isnan(cor) * not_nans
@@ -198,9 +205,12 @@ def rxn_concentration_make_observers(behavior_epochs):
             n_cor = np.sum(cor[idx])
             res[i,:] = n_cor, n_obs
             rx2[i] = r
+        res = res[rx2 < 250, :]
+        rx2 = rx2[rx2 < 250]
         obs = psychophysics.Observer(res, rx2)
         obs.concentration = con
         observers.append(obs)
+
     return observers
 
 
