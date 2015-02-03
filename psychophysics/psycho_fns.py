49f3051dd0f4459f5ed557788675847af4a5c470
Chris Wilson <cdw291@nyu.edu>
2015-02-03 13:58:01 -0500

    no numba

--- psychophysics/psycho_fns.py
+++ psychophysics/psycho_fns.py
@@ -2,7 +2,6 @@ from __future__ import division
 __author__ = 'chris'
 
 import numpy as np
-import numba
 
 def weibull(i, alpha, beta, guess, lapse):
     """
@@ -17,13 +16,11 @@ def weibull(i, alpha, beta, guess, lapse):
     return ((1.-lapse)-(1.-guess-lapse) *
             np.exp(-(i/alpha)**beta))
 
-@numba.autojit('f8[:](f8[:], f8, f8, f8, f8)')
 def logistic(i, alpha, beta, guess, lapse):
-    temp = (1.-lapse-guess)
-    result = np.zeros(i.shape)
-    for ii in xrange(len(i)):
-        result[ii] = guess + temp / (1. + np.exp(-beta*(i[ii]-alpha)))
-    return result
+    return (guess+(1.-lapse-guess)/
+            (1.+np.exp(-beta*(i-alpha))))
+
+
 
 p_funcs = {'Weibull' : weibull,
            'logistic' : logistic,}
