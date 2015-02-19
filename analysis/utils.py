d31a93234070e64f752988f96cc9bf24ba68e959
Chris Wilson <cdw291@nyu.edu>
2015-02-19 09:51:49 -0500

    Add functionality to get last n sessions

--- analysis/utils.py
+++ analysis/utils.py
@@ -15,6 +15,7 @@ def get_session_fns(mouse, sessions=(), base_directory=BASE_DIRECTORY):
 
     :param mouse: Int animal id number
     :param sessions: List of session numbers. If empty, will return ALL sessions for given mouse.
+    negative values return the last n sessions for the animal.
     :param base_directory: directory where the Data structure lives.
     :type mouse: int
     :type sessions: tuple
@@ -33,11 +34,25 @@ def get_session_fns(mouse, sessions=(), base_directory=BASE_DIRECTORY):
         sessions = list(sessions)
 
     if len(sessions) == 0:
-        d = os.listdir(mousepath)
-        for fn in d:
+        dirl = os.listdir(mousepath)
+        for fn in dirl:
             m, s, d = parse_h5path(fn)
             if s:
                 sessions.append(s)
+    elif sessions[0] < 0:
+        dirl = os.listdir(mousepath)
+        sess = []
+        for i in xrange(-sessions[0]):
+            s_max = 0
+            for fn in dirl:
+                m, s, d = parse_h5path(fn)
+                if s > s_max and s not in sess:
+                    s_max = s
+            sess.append(s_max)
+        sess.sort()
+        sessions = sess
+        print sessions
+
     # Once session list is built go through the session numbers and return the filenames that occur with them.
     for session in sessions:
         fn = _get_session_fn(mouse, session, base_directory)
@@ -86,25 +101,30 @@ def parse_h5path(path):
     :type path: str
     """
     fn = os.path.split(path)[1]  # this returns the filename out of the entire path, even if only filename is provided.
-    if fn.find('mouse') > -1:  # CAN CHANGE THE FORMATTING HERE!
-        mouse_start = fn.find('mouse') + len('mouse')
-    else:
-        return None, None, None
-    mouse_end = fn.find('_sess')
-    mouse = fn[mouse_start:mouse_end]
-    sess_start = mouse_end + len('_sess')
-    sess_end = fn.find('_D')
-    sess = fn[sess_start:sess_end]
-    date_start = sess_end + len('_D')
-    date_end = fn.find('.h5')
-    dt_str = fn[date_start:date_end]
-    dt_l = dt_str.split('T')
-    date_str = dt_l[0]
-    time_str = dt_l[1]
-    date_str_l = date_str.split('_')
-    time_str_l = time_str.split('_')
-    date_int_l = [int(x) for x in date_str_l]
-    time_int_l = [int(x) for x in time_str_l]
-    dt_final = date_int_l + time_int_l  # makes a list of integers: [Y, M, D, H, M, S]
-    date_obj = datetime.datetime(*dt_final)  # make a datetime object by unpacking this list.
-    return int(mouse), int(sess), date_obj
+    try:
+        if fn.find('mouse') > -1:  # CAN CHANGE THE FORMATTING HERE!
+            mouse_start = fn.find('mouse') + len('mouse')
+        else:
+            return None, None, None
+        mouse_end = fn.find('_sess')
+        mouse = int(fn[mouse_start:mouse_end])
+        sess_start = mouse_end + len('_sess')
+        sess_end = fn.find('_D')
+        sess = int(fn[sess_start:sess_end])
+        date_start = sess_end + len('_D')
+        date_end = fn.find('.h5')
+        dt_str = fn[date_start:date_end]
+        dt_l = dt_str.split('T')
+        date_str = dt_l[0]
+        time_str = dt_l[1]
+        date_str_l = date_str.split('_')
+        time_str_l = time_str.split('_')
+        date_int_l = [int(x) for x in date_str_l]
+        time_int_l = [int(x) for x in time_str_l]
+        dt_final = date_int_l + time_int_l  # makes a list of integers: [Y, M, D, H, M, S]
+        date_obj = datetime.datetime(*dt_final)  # make a datetime object by unpacking this list.
+    except Exception as e:
+        print "PROBLEM WITH PARSING OF FILENAME: {0}".format(fn)
+        raise e
+
+    return mouse, sess, date_obj
